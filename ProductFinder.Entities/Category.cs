b0f24fa6297d6397fd2ee1bea72b544354d37c3f
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:18:23 +0300

    ctaegory entity

---
+++ ProductFinder.Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductFinder.Entities
+{
+	public class Category
+	{
+	    [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int CategoryId { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+		public List<Product> Product { get; set; }
+	}
+}
+
