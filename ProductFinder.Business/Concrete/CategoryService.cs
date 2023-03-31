428c2db738caa08baf3c898d66d5d8dc320121f9
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:13:37 +0300

    concrete service class

---
+++ ProductFinder.Business/Concrete/CategoryService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ProductFinder.Business.Abstract;
+using ProductFinder.DataAccess.Abstract;
+using ProductFinder.DataAccess.Concrete;
+using ProductFinder.Entities;
+
+namespace ProductFinder.Business.Concrete
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+
+        }
+
+        public Category CreateCategory(Category category)
+        {
+            return _categoryRepository.CreateCategory(category);
+        }
+
+        public void DeleteCategory(int Id)
+        {
+            _categoryRepository.DeleteCategory(Id);
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _categoryRepository.GetAllCategories();
+        }
+
+        public Category GetCategoryById(int Id)
+        {
+            if (Id > 0)
+            {
+                return _categoryRepository.GetCategoryById(Id);
+            }
+            else
+            {
+                throw new Exception("Id cannot be less than 1");
+            }
+        }
+
+        public Category UpdateCategory(Category category)
+        {
+            return _categoryRepository.UpdateCategory(category);
+        }
+    }
+}
+
---
+++ ProductFinder.Business/Concrete/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ProductFinder.Business.Abstract;
+using ProductFinder.DataAccess.Abstract;
+using ProductFinder.DataAccess.Concrete;
+using ProductFinder.Entities;
+
+namespace ProductFinder.Business.Concrete
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+
+        }
+        public Product CreateProduct(Product product)   
+        {
+            return _productRepository.CreateProduct(product);
+        }
+
+        public void DeleteProduct(int Id)
+        {
+            _productRepository.DeleteProduct(Id);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _productRepository.GetAllProducts();
+        }
+
+        public Product GetProductById(int Id)
+        {
+            if (Id > 0) {
+                return _productRepository.GetProductById(Id);
+            }
+            else {
+                throw new Exception("Id cannot be less than 1");
+            }
+            
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            return _productRepository.UpdateProduct(product);
+        }
+    }
+}
+
