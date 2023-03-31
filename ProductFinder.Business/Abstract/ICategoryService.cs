5f1b959049dd0b09d18574d91736f6be02576cf7
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:11:07 +0300

    service interface

---
+++ ProductFinder.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProductFinder.Entities;
+
+namespace ProductFinder.Business.Abstract
+{
+	public interface ICategoryService
+	{
+        List<Category> GetAllCategories();
+
+        Category GetCategoryById(int Id);
+
+        Category CreateCategory(Category category);
+
+        Category UpdateCategory(Category category);
+
+        void DeleteCategory(int Id);
+
+    }
+}
+
