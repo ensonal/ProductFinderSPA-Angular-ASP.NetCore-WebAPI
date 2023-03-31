a274ef5e3f731bcf05b55837cf14ad1f46884a6a
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:15:28 +0300

    db contex class for product

---
+++ ProductFinder.DataAccess/ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProductFinder.Entities;
+
+namespace ProductFinder.DataAccess
+{
+	public class ProductDbContext : DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost; Database=ProductDb; uid=SA; pwd=reallyStrongPwd123; TrustServerCertificate=true");
+
+            
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
+
