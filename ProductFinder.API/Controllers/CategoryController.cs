6247ff19b980be48a965c89726fdeb691f24e5d3
Enes Önal <97753783+ensonal@users.noreply.github.com>
2023-03-31 05:01:38 +0300

    Add files via upload

---
+++ ProductFinder.API/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductFinder.Business.Abstract;
+using ProductFinder.Business.Concrete;
+using ProductFinder.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductFinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public List<Category> Get()
+        {
+            return _categoryService.GetAllCategories();
+        }
+
+        [HttpGet("{Id}")]
+        public Category Get(int Id)
+        {
+            return _categoryService.GetCategoryById(Id);
+        }
+
+        [HttpPost]
+        public Category Post([FromBody] Category category)
+        {
+            return _categoryService.CreateCategory(category);
+        }
+
+        [HttpPut]
+        public Category Put([FromBody] Category category)
+        {
+            return _categoryService.UpdateCategory(category);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int Id)
+        {
+            _categoryService.DeleteCategory(Id);
+        }
+
+
+
+
+        /*
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        */
+    }
+}
+
---
+++ ProductFinder.API/Program.cs
@@ -0,0 +1,43 @@
+﻿using ProductFinder.Business.Abstract;
+using ProductFinder.Business.Concrete;
+using ProductFinder.DataAccess.Abstract;
+using ProductFinder.DataAccess.Concrete;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IProductService, ProductService>();
+builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+builder.Services.AddSingleton<ICategoryService, CategoryService>();
+builder.Services.AddSingleton<ICategoryRepository, CategoryReposityory>();
+builder.Services.AddCors(options => options.AddPolicy(name: "ProductOrigins",
+    policy => {
+        policy.WithOrigins("http://localhost:50502").AllowAnyMethod().AllowAnyHeader();
+    })); 
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseCors("ProductOrigins");
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapControllers();
+
+app.Run();
+
