150da90d1964c3cf0a96cf2df843e625d075f1db
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:16:49 +0300

    concrete repo class

---
+++ ProductFinder.DataAccess/Concrete/CategoryReposityory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using ProductFinder.DataAccess.Abstract;
+using ProductFinder.Entities;
+namespace ProductFinder.DataAccess.Concrete
+{
+	public class CategoryReposityory : ICategoryRepository
+	{
+        public Category CreateCategory(Category category)
+        {
+            using (var categoryDbContext = new CategoryDbContext())
+            {
+                categoryDbContext.Categories.Add(category);
+                categoryDbContext.SaveChanges();
+                return category;
+            }
+        }
+
+       
+        public Category GetCategoryById(int Id)
+        {
+            using (var categoryDbContext = new CategoryDbContext())
+            {
+                return categoryDbContext.Categories.Find(Id);
+
+            }
+        }
+
+        public void DeleteCategory(int Id)
+        {
+            using (var categoryDbContext = new CategoryDbContext())
+            {
+                var deletedCategory = GetCategoryById(Id);
+                categoryDbContext.Categories.Remove(deletedCategory);
+                categoryDbContext.SaveChanges();
+            }
+
+
+        }
+
+        public Category UpdateCategory(Category category)
+        {
+            using (var categoryDbContext = new CategoryDbContext())
+            {
+                categoryDbContext.Categories.Update(category);
+                categoryDbContext.SaveChanges();
+                return category;
+            }
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            using (var categoryDbContext = new CategoryDbContext())
+            {
+                return categoryDbContext.Categories.ToList();
+
+            }
+        }
+    }
+}
+
---
+++ ProductFinder.DataAccess/Concrete/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ProductFinder.DataAccess.Abstract;
+using ProductFinder.Entities;
+
+namespace ProductFinder.DataAccess.Concrete
+{
+	public class ProductRepository:IProductRepository
+	{
+       
+        public Product CreateProduct(Product product)
+        {
+            using (var productDbContext = new ProductDbContext())
+            {
+                productDbContext.Product.Add(product);
+                productDbContext.SaveChanges();
+                return product;
+            }
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            using (var productDbContext = new ProductDbContext())
+            {
+                return productDbContext.Product.ToList();
+
+            }
+        }
+
+        public Product GetProductById(int Id)
+        {
+            using(var productDbContext = new ProductDbContext())
+            {
+                return productDbContext.Product.Find(Id);
+
+            }
+        }
+
+        public void DeleteProduct(int Id)
+        {
+            using(var productDbContext = new ProductDbContext())
+            {
+                var deletedProduct = GetProductById(Id);
+                productDbContext.Product.Remove(deletedProduct);
+                productDbContext.SaveChanges();
+            }
+           
+            
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            using (var productDbContext = new ProductDbContext())
+            {
+                productDbContext.Product.Update(product);
+                productDbContext.SaveChanges();
+                return product;
+            }
+        }
+    }   
+}
+
