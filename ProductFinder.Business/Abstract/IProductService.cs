4e969ce28421b8dcf9d0ad9e08f9f6dba7ea2b62
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:11:16 +0300

    service interface

---
+++ ProductFinder.Business/Abstract/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ProductFinder.Entities;
+
+namespace ProductFinder.Business.Abstract
+{
+	public interface IProductService
+	{
+		List<Product> GetAllProducts();
+
+        Product GetProductById(int Id);
+
+        Product CreateProduct(Product product);
+
+        Product UpdateProduct(Product product);
+
+        void DeleteProduct(int Id);
+
+
+    }
+}
+
