a8ef03553410852c50bacf1e5483ec35d6e9f642
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:18:32 +0300

    product entity

---
+++ ProductFinder.Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductFinder.Entities
+{
+    public class Product
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(250)]
+        public string Description { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+
+}
+
