bab04b80f3a1dc4dc0f27a2cbfe7b895d0d841c9
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:17:12 +0300

    db context for category

---
+++ ProductFinder.DataAccess/CategoryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProductFinder.Entities;
+
+namespace ProductFinder.DataAccess
+{
+	public class CategoryDbContext : DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost; Database=ProductDb; uid=SA; pwd=reallyStrongPwd123; TrustServerCertificate=true");
+
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
