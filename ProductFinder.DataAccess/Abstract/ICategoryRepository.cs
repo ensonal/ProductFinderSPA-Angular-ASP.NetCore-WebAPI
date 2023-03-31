3b0963fcd6d0ddb0f0a38764cb1b5bbe8ef6f53b
Enes Önal <enes.onal777@gmail.com>
2023-03-31 05:14:47 +0300

    interface repo class

---
+++ ProductFinder.DataAccess/Abstract/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ProductFinder.Entities;
+namespace ProductFinder.DataAccess.Abstract
+{
+	public interface ICategoryRepository
+	{
+        List<Category> GetAllCategories();
+
+        Category GetCategoryById(int Id);
+
+        Category CreateCategory(Category Category);
+
+        Category UpdateCategory(Category Category);
+
+        void DeleteCategory(int Id);
+    }
+}
+
---
+++ ProductFinder.DataAccess/Abstract/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ProductFinder.Entities;
+
+namespace ProductFinder.DataAccess.Abstract
+{
+	public interface IProductRepository
+	{
+		List<Product> GetAllProducts();
+
+		Product GetProductById(int Id);
+
+		Product CreateProduct(Product product);
+
+		Product UpdateProduct(Product product);
+
+		void DeleteProduct(int Id);
+	}
+}
+
